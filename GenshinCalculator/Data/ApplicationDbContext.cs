664d9efe9780174a3510c4330e181d959ce180eb
PaulHaig <gaagpavel01@gmail.com>
2022-06-17 20:25:24 +0300

    Настроил проект
    Настроил проект для работы с БД, загрузил библиотеки для работы с ней.

---
+++ GenshinCalculator/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GenshinCalculator.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
