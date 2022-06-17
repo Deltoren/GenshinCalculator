3c60d74e97e6aee4679024ff3c9c7f961acd745f
PaulHaig <gaagpavel01@gmail.com>
2022-06-17 21:00:08 +0300

    Доработал MVC сущности Персонаж
    - Создал ViewModels создания и редактирования
    - Доработал отображение Create и Edit
    - Доработал методы контроллера

--- GenshinCalculator/Controllers/CharactersController.cs
+++ GenshinCalculator/Controllers/CharactersController.cs
@@ -7,35 +7,36 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using GenshinCalculator.Data;
 using GenshinCalculator.Models;
+using GenshinCalculator.Models.ViewModels;
 
 namespace GenshinCalculator.Controllers
 {
     public class CharactersController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext context;
 
         public CharactersController(ApplicationDbContext context)
         {
-            _context = context;
+            this.context = context;
         }
 
         // GET: Characters
         public async Task<IActionResult> Index()
         {
-              return _context.Characters != null ? 
-                          View(await _context.Characters.ToListAsync()) :
+              return context.Characters != null ? 
+                          View(await context.Characters.ToListAsync()) :
                           Problem("Entity set 'ApplicationDbContext.Characters'  is null.");
         }
 
         // GET: Characters/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null || _context.Characters == null)
+            if (id == null || context.Characters == null)
             {
                 return NotFound();
             }
 
-            var character = await _context.Characters
+            var character = await context.Characters
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (character == null)
             {
@@ -48,7 +49,7 @@ namespace GenshinCalculator.Controllers
         // GET: Characters/Create
         public IActionResult Create()
         {
-            return View();
+            return View(new CharacterCreateModel());
         }
 
         // POST: Characters/Create
@@ -56,31 +57,60 @@ namespace GenshinCalculator.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Fullname,AvatarPath,Rarity,Description,DayOfBirth")] Character character)
+        public async Task<IActionResult> Create(CharacterCreateModel model)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(character);
-                await _context.SaveChangesAsync();
+                var character = new Character
+                {
+                    Name = model.Name,
+                    Fullname = model.Fullname,
+                    AvatarPath = model.AvatarPath,
+                    Rarity = model.Rarity,
+                    Description = model.Description,
+                    DayOfBirth = model.DayOfBirth,
+                    Vision = model.Vision,
+                    WeaponType = model.WeaponType
+                };
+
+                context.Add(character);
+                await context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(character);
+            return View(model);
         }
 
         // GET: Characters/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null || _context.Characters == null)
+            if (id == null)
             {
                 return NotFound();
             }
 
-            var character = await _context.Characters.FindAsync(id);
+            var character = await context.Characters
+                .SingleOrDefaultAsync(y => y.Id == id);
+
             if (character == null)
             {
                 return NotFound();
             }
-            return View(character);
+
+            var model = new CharacterEditModel
+            {
+                Name = character.Name,
+                Fullname = character.Fullname,
+                AvatarPath = character.AvatarPath,
+                Rarity = character.Rarity,
+                Description = character.Description,
+                DayOfBirth = character.DayOfBirth,
+                Vision = character.Vision,
+                WeaponType = character.WeaponType
+            };
+
+            ViewBag.Id = character.Id;
+
+            return View(model);
         }
 
         // POST: Characters/Edit/5
@@ -88,45 +118,46 @@ namespace GenshinCalculator.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Fullname,AvatarPath,Rarity,Description,DayOfBirth")] Character character)
+        public async Task<IActionResult> Edit(int? id, CharacterEditModel model)
         {
-            if (id != character.Id)
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var character = await context.Characters
+                .SingleOrDefaultAsync(y => y.Id == id);
+
+            if (character == null)
             {
                 return NotFound();
             }
 
             if (ModelState.IsValid)
             {
-                try
-                {
-                    _context.Update(character);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CharacterExists(character.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                character.Name = model.Name;
+                character.Fullname = model.Fullname;
+                character.AvatarPath = model.AvatarPath;
+                character.Rarity = model.Rarity;
+                character.Description = model.Description;
+                character.DayOfBirth = model.DayOfBirth;
+                character.Vision = model.Vision;
+                character.WeaponType = model.WeaponType;
             }
-            return View(character);
+
+            await context.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
 
         // GET: Characters/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null || _context.Characters == null)
+            if (id == null || context.Characters == null)
             {
                 return NotFound();
             }
 
-            var character = await _context.Characters
+            var character = await context.Characters
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (character == null)
             {
@@ -141,23 +172,23 @@ namespace GenshinCalculator.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.Characters == null)
+            if (context.Characters == null)
             {
                 return Problem("Entity set 'ApplicationDbContext.Characters'  is null.");
             }
-            var character = await _context.Characters.FindAsync(id);
+            var character = await context.Characters.FindAsync(id);
             if (character != null)
             {
-                _context.Characters.Remove(character);
+                context.Characters.Remove(character);
             }
             
-            await _context.SaveChangesAsync();
+            await context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool CharacterExists(int id)
         {
-          return (_context.Characters?.Any(e => e.Id == id)).GetValueOrDefault();
+          return (context.Characters?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
---
+++ GenshinCalculator/Models/ViewModels/CharacterEditModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GenshinCalculator.Models.ViewModels
+{
+    public class CharacterEditModel
+    {
+        public string Name { get; set; }
+        public string? Fullname { get; set; }
+        public string? AvatarPath { get; set; }
+        public int Rarity { get; set; }
+        public string? Description { get; set; }
+        public DateTime DayOfBirth { get; set; }
+        public Vision Vision { get; set; }
+        public WeaponType WeaponType { get; set; }
+    }
+}
