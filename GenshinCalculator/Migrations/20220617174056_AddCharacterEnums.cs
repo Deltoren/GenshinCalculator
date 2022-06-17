a17f3f78200906e651c75ae2df5bddf597d2518e
PaulHaig <gaagpavel01@gmail.com>
2022-06-17 20:42:34 +0300

    Добавил поля определённых enum в модель
    Добавил поля типа Vision и WeaponType в модель сущности Character.
    Инициализировал миграцию для применения изменений.

---
+++ GenshinCalculator/Migrations/20220617174056_AddCharacterEnums.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GenshinCalculator.Migrations
+{
+    public partial class AddCharacterEnums : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Vision",
+                table: "Characters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WeaponType",
+                table: "Characters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Vision",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "WeaponType",
+                table: "Characters");
+        }
+    }
+}
--- GenshinCalculator/Models/Character.cs
+++ GenshinCalculator/Models/Character.cs
@@ -9,5 +9,7 @@
         public int Rarity { get; set; }
         public string? Description { get; set; }
         public DateTime DayOfBirth { get; set; }
+        public Vision Vision { get; set; }
+        public WeaponType WeaponType { get; set; }
     }
 }
