bc85de4a53de3333c3715000f6abb3e578276841
PaulHaig <gaagpavel01@gmail.com>
2022-06-17 20:40:02 +0300

    Создал первичный набор MVC для сущности Character
    Создал модель, на её основе сгенерировал контроллер и представления.
    Инициализировал миграцию.

---
+++ GenshinCalculator/Migrations/20220617173501_AddCharacters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GenshinCalculator.Migrations
+{
+    public partial class AddCharacters : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Characters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fullname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AvatarPath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rarity = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DayOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Characters", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Characters");
+        }
+    }
+}
---
+++ GenshinCalculator/Models/Character.cs
@@ -0,0 +1,13 @@
+﻿namespace GenshinCalculator.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Fullname { get; set; }
+        public string? AvatarPath { get; set; }
+        public int Rarity { get; set; }
+        public string? Description { get; set; }
+        public DateTime DayOfBirth { get; set; }
+    }
+}
