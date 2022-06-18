97776c28684fd413fa81f74683c9ace68479a1dd
PaulHaig <gaagpavel01@gmail.com>
2022-06-18 15:34:54 +0300

    Доработал MVC сущности
    Внёс изменения в контроллер и представления.

--- GenshinCalculator/Controllers/WeaponsController.cs
+++ GenshinCalculator/Controllers/WeaponsController.cs
@@ -7,35 +7,36 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using GenshinCalculator.Data;
 using GenshinCalculator.Models;
+using GenshinCalculator.Models.ViewModels;
 
 namespace GenshinCalculator.Controllers
 {
     public class WeaponsController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext context;
 
         public WeaponsController(ApplicationDbContext context)
         {
-            _context = context;
+            this.context = context;
         }
 
         // GET: Weapons
         public async Task<IActionResult> Index()
         {
-              return _context.Weapons != null ? 
-                          View(await _context.Weapons.ToListAsync()) :
+              return context.Weapons != null ? 
+                          View(await context.Weapons.ToListAsync()) :
                           Problem("Entity set 'ApplicationDbContext.Weapons'  is null.");
         }
 
         // GET: Weapons/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null || _context.Weapons == null)
+            if (id == null || context.Weapons == null)
             {
                 return NotFound();
             }
 
-            var weapon = await _context.Weapons
+            var weapon = await context.Weapons
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (weapon == null)
             {
@@ -48,7 +49,7 @@ namespace GenshinCalculator.Controllers
         // GET: Weapons/Create
         public IActionResult Create()
         {
-            return View();
+            return View(new WeaponCreateModel());
         }
 
         // POST: Weapons/Create
@@ -56,31 +57,46 @@ namespace GenshinCalculator.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Rarity,ImagePath,WeaponType")] Weapon weapon)
+        public async Task<IActionResult> Create(WeaponCreateModel model)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(weapon);
-                await _context.SaveChangesAsync();
+                var weapon = new Weapon
+                {
+                    Name = model.Name,
+                    Rarity = model.Rarity,
+                    ImagePath = model.ImagePath,
+                    WeaponType = model.WeaponType
+                };
+                context.Add(weapon);
+                await context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(weapon);
+            return View(model);
         }
 
         // GET: Weapons/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null || _context.Weapons == null)
+            if (id == null)
             {
                 return NotFound();
             }
-
-            var weapon = await _context.Weapons.FindAsync(id);
+            var weapon = await context.Weapons
+                .SingleOrDefaultAsync(y => y.Id == id);
             if (weapon == null)
             {
                 return NotFound();
             }
-            return View(weapon);
+            var model = new WeaponEditModel
+            {
+                Name = weapon.Name,
+                Rarity = weapon.Rarity,
+                ImagePath = weapon.ImagePath,
+                WeaponType = weapon.WeaponType
+            };
+            ViewBag.Id = weapon.Id;
+            return View(model);
         }
 
         // POST: Weapons/Edit/5
@@ -88,45 +104,38 @@ namespace GenshinCalculator.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Rarity,ImagePath,WeaponType")] Weapon weapon)
+        public async Task<IActionResult> Edit(int? id, WeaponEditModel model)
         {
-            if (id != weapon.Id)
+            if (id == null)
             {
                 return NotFound();
             }
-
+            var weapon = await context.Weapons
+                .SingleOrDefaultAsync(y => y.Id == id);
+            if (weapon == null)
+            {
+                return NotFound();
+            }
             if (ModelState.IsValid)
             {
-                try
-                {
-                    _context.Update(weapon);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!WeaponExists(weapon.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                weapon.Name = model.Name;
+                weapon.Rarity = model.Rarity;
+                weapon.ImagePath = model.ImagePath;
+                weapon.WeaponType = model.WeaponType;
             }
-            return View(weapon);
+            await context.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
 
         // GET: Weapons/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null || _context.Weapons == null)
+            if (id == null || context.Weapons == null)
             {
                 return NotFound();
             }
 
-            var weapon = await _context.Weapons
+            var weapon = await context.Weapons
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (weapon == null)
             {
@@ -141,23 +150,23 @@ namespace GenshinCalculator.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.Weapons == null)
+            if (context.Weapons == null)
             {
                 return Problem("Entity set 'ApplicationDbContext.Weapons'  is null.");
             }
-            var weapon = await _context.Weapons.FindAsync(id);
+            var weapon = await context.Weapons.FindAsync(id);
             if (weapon != null)
             {
-                _context.Weapons.Remove(weapon);
+                context.Weapons.Remove(weapon);
             }
             
-            await _context.SaveChangesAsync();
+            await context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool WeaponExists(int id)
         {
-          return (_context.Weapons?.Any(e => e.Id == id)).GetValueOrDefault();
+          return (context.Weapons?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
---
+++ GenshinCalculator/Models/ViewModels/WeaponEditModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GenshinCalculator.Models.ViewModels
+{
+    public class WeaponEditModel
+    {
+        public string Name { get; set; }
+        public int Rarity { get; set; }
+        public string? ImagePath { get; set; }
+        public WeaponType WeaponType { get; set; }
+    }
+}
